36c74ce0d5c76378825b8e4146c04793bb9845bc
LucasSouzaBorges <lucas.borges90@etec.sp.gov.br>
2022-04-26 12:36:13 -0300

    L

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <html>
     <head>
-        <title>Formulario</title>
+        <title>Formulário</title>
     </head>
     <body>
-        <form action="Resultado.class.php" method="POST">
-            <label for="lblNome">Nome</label>
-            <input type="text" nome="txtNome"><br>
-            <label for="lblEmail">Email</label>
-            <input type="text" nome="txtEmail"><br>
+        <h1>Cadastro Pessoa Física</h1><br>
+        <form method="GET" action="controller.php">
+            <label for="lblNome">Nome:</label><br>
+            <input type="text" name="txtNome"><br>
+            <label for="lblEndereco">Endereço:</label><br>
+            <input type="text" name="txtEndereco"><br>
+            <label for="lblBairro">Bairro:</label><br>
+            <input type="text" name="txtBairro"><br>
+            <label for="lblCep">Cep:</label><br>
+            <input type="text" name="txtCep"><br>
             <button>Cadastrar</button>
         </form>
     </body>
